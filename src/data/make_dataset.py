f0da65cc4f0919e305a8722ed13366d69513711c
Ubuntu <j.jonusas@gmail.com>
2020-08-12 14:42:55 +0000

    Add a function for downsampling a dataframe

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -137,3 +137,43 @@ def remove_clipping_with_flexible_window(
               "% of the data has been removed!")
 
     return time_series_df
+
+
+def downsample_dataframe(df, offset='D', clip_method=None, night_method=None):
+    """Downsamples the dataframe.
+
+    Downsamples the DataFrame using inbuild resample function of pandas. The
+    function has options for removing clipping and night time data. The
+    argument <night_methods> can be set to None or 'basic' to either not remove
+    any nighttime data or remove all of it, respectively.  The argument
+    <clip_method> can be set to None, 'universal', or 'flexible'. The methods
+    remove no clipping data, remove clipping data with unversal window, or
+    remove clipping data with flexible window respectively. If the optional
+    arguments are not speficied, the function downsample to days with no data
+    removal.
+
+    Args:
+        df (Pandas DataFrame): a DataFrame with timedate as index
+        offset (str): Optional; offset string representing target downsampling
+        clip_method (str): Optional; string representing clipping removal method 
+        night_method (str): Optional; string representing night removal method 
+
+
+    Returns:
+        Pandas DataFrame: downsampled dataframe
+    """
+    
+    if night_method == 'basic':
+        df = remove_night_time_data(df)
+    elif night_method is not None:
+        print("Error: night_method needs to be None, or 'basic'")
+
+    if clip_method == 'universal':
+        df = remove_clipping_with_universal_window(df)
+    elif clip_method == 'flexible':
+        df = remove_clipping_with_flexible_window(df)
+    elif clip_method is not None:
+        print(("Error: clip_method needs to be None, 'universal', or \
+                'flexible'"))
+    
+    return df.resample(offset).mean()
