19e36a503c032c102dee961761181b1be4add54c
clemens <clemens@jochum.email>
2020-08-13 13:40:41 +0200

    removed year, month, etc. columns, added import_df_from_pickle_zip function

--- src/data/import_data.py
+++ src/data/import_data.py
@@ -8,7 +8,8 @@ import zipfile as zf
 import time
 import pandas as pd
 
-def import_df_from_zip(path_to_zip, csv_index, verbose=False):
+
+def import_df_from_zip(path_to_zip, csv_index, datetime=True, verbose=False):
 
     """
     Import a timeseries from a zipped datafile
@@ -18,7 +19,8 @@ def import_df_from_zip(path_to_zip, csv_index, verbose=False):
         Args:
             path_to_zip (str): path to the zip-file containing the dataset
             csv_index (int): index (0-49) of the desired csv-file
-            verbose (bool): optional, print output if true
+            datetime (bool, optional): parse time-string to datetime, defaults to True
+            verbose (bool, optional): print output if true, defaults to False
 
         Returns:
             data_frame (Pandas DataFrame): dataframe converted from csv
@@ -33,37 +35,36 @@ def import_df_from_zip(path_to_zip, csv_index, verbose=False):
     data_frame = pd.read_csv(
         zip_file.open(csv_path),
         index_col='Unnamed: 0',
-        parse_dates=True,
         )
+
+    if datetime is True:
+        data_frame.index = pd.to_datetime(data_frame.index)
 
     # print how much time read_csv needs
     time_01 = time.time()
     if verbose is True:
         print("time for importing dataframe: {:.2f} seconds".format(time_01 - time_00))
 
-    # add new columns with year, month, day, hour of timestamp
-    data_frame['year'] = data_frame.index.year
-    data_frame['month'] = data_frame.index.month
-    data_frame['day'] = data_frame.index.day
-    data_frame['hour'] = data_frame.index.hour
-    data_frame['minute_of_hour'] = data_frame.index.minute
-    data_frame['minute_of_day'] = data_frame['minute_of_hour'] + data_frame['hour'] * 60
+    # add new column for minute of day
+    if datetime is True:
+        data_frame['minute_of_hour'] = data_frame.index.minute + data_frame.index.hour * 60
 
     # return dataframe
     return data_frame
 
 
-def import_df_from_dir(path_to_dir, csv_index, verbose=False):
+def import_df_from_dir(path_to_dir, csv_index, datetime=True, verbose=False):
 
     """
     Import a timeseries from a data directory
-    
+
     Takes csv file from the destination directory and returns a formatted dataframe.
 
         Args:
             path_to_dir (str): path to the directory containing the csv-files
             csv_index (int): index (0-49) of the desired csv-file
-            verbose (bool): optional, print output if true
+            datetime (bool, optional): parse time-string to datetime, defaults to True
+            verbose (bool, optional): print output if true, defaults to False
 
         Returns:
             data_frame (Pandas DataFrame): dataframe converted from csv
@@ -80,21 +81,51 @@ def import_df_from_dir(path_to_dir, csv_index, verbose=False):
     data_frame = pd.read_csv(
         csv_path,
         index_col='Unnamed: 0',
-        parse_dates=True,
         )
+
+    if datetime is True:
+        data_frame.index = pd.to_datetime(data_frame.index)
 
     # print how much time read_csv needs
     time_01 = time.time()
     if verbose is True:
         print("time for importing dataframe: {:.2f} seconds".format(time_01 - time_00))
 
-    # add new columns with year, month, day, hour of timestamp
-    data_frame['year'] = data_frame.index.year
-    data_frame['month'] = data_frame.index.month
-    data_frame['day'] = data_frame.index.day
-    data_frame['hour'] = data_frame.index.hour
-    data_frame['minute_of_hour'] = data_frame.index.minute
-    data_frame['minute_of_day'] = data_frame['minute_of_hour'] + data_frame['hour'] * 60
+    # add new column for minute of day
+    if datetime is True:
+        data_frame['minute_of_hour'] = data_frame.index.minute + data_frame.index.hour * 60
+
+    # return dataframe
+    return data_frame
+
+def import_df_from_pickle_zip(path_to_zip, csv_index, verbose=False):
+
+    """
+    Import a timeseries from a zipped pickled dataframe
+
+    Extracts a dataframe file from the pickle (compressed using gzip)
+    which is saved within a zipped folder
+
+        Args:
+            path_to_zip (str): path to the zip-file containing the pickled dataframes
+            csv_index (int): index (0-49) of the desired csv-file
+            verbose (bool, optional): print output if true, defaults to False
+
+        Returns:
+            data_frame (Pandas DataFrame): unpickled dataframe
+    """
+
+    # unpickle df from within within zip
+    time_00 = time.time()
+    zip_file = zf.ZipFile(path_to_zip)
+    filename_prefix = os.path.basename(os.path.splitext(path_to_zip)[0])
+    csv_path = '{0:s}/{0:s}_{1:d}.csv'.format(filename_prefix, csv_index)
+    data_frame = pd.read_pickle(zip_file.open(csv_path), compression='gzip')
+    time_01 = time.time()
+
+    # print how much time read_pickle needs
+    if verbose is True:
+        print("time for importing dataframe: {:.2f} seconds".format(time_01 - time_00))
 
     # return dataframe
     return data_frame
