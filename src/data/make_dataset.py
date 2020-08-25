0483e21820cb84622fecffa5023216d7f03733f0
clemens <c.jochum@gmail.com>
2020-08-25 09:04:39 +0200

    wrote normalization function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -71,6 +71,56 @@ def remove_clipping_with_universal_window(
     return time_series_df
 
 
+def return_universal_clipping_window(
+    time_series_df, verbose=False, max_power=None
+):
+    """return universal clipping window of power signal.
+
+    The function determines a time window within a day when, over the span
+    of the time series, each minute goes through at least one day that hits
+    the maximum power beyond which power is maxed out.
+
+    Args:
+        time_series_df (Pandas DataFrame): with a time stamp (datetime) as an
+            index, a "Power" column (float), and a "minute_of_day" (int) column
+        verbose (bool): Optional; print additional info if true
+        max_power (float): Optional; specify maximal power
+
+    Returns:
+        Tupel: limits of the universal window.
+    """
+    if not max_power:
+        max_power = time_series_df.Power.max()
+        if verbose:
+            print("Max power set as "+str(max_power))
+
+    # To measure at the end how much data is removed
+    initial_size = len(time_series_df)
+
+    clipping_df = time_series_df[time_series_df.Power >= max_power]
+    if clipping_df.empty:
+        if verbose:
+            print("Power never exceeds max value; no data removed.")
+        return time_series_df
+    else:
+        window_first_minute = clipping_df.minute_of_day.min()
+        window_last_minute = clipping_df.minute_of_day.max()
+
+    # Removing the fixed window
+    time_series_df = time_series_df[
+        (time_series_df.minute_of_day < window_first_minute) |
+        (time_series_df.minute_of_day > window_last_minute)
+        ]
+
+    if verbose:
+        print(
+            str(100 * (1 - len(time_series_df)/initial_size)) +
+            "% of the data has been removed!"
+        )
+
+    return (window_first_minute, window_last_minute)
+
+
 def remove_clipping_with_flexible_window(
     time_series_df, verbose=False, max_power=None
 ):
@@ -100,7 +150,7 @@ def remove_clipping_with_flexible_window(
     minute_of_day = np.array(time_series_df.minute_of_day)
     power = np.array(time_series_df.Power)
     windows = np.zeros((2, 366))
-    found_non_empyt_window = False
+    found_non_empty_window = False
 
     # To measure at the end how much data is removed
     if verbose:
@@ -112,20 +162,20 @@ def remove_clipping_with_flexible_window(
                 (power == max_power)
             ]
         if clipping_df.empty:
-            windows[0][day] = -1
+            windows[0][day] = -2
             windows[1][day] = -1
         else:
             windows[0][day] = clipping_df.minute_of_day.min()
             windows[1][day] = clipping_df.minute_of_day.max()
-            found_non_empyt_window = True
+            found_non_empty_window = True
 
-    if not found_non_empyt_window:
-        # No non empty window was found
+    if not found_non_empty_window:
+        # no non empty window was found
         if verbose:
-            print("Power never exceeds max value; no data removed.")
+            print("power never exceeds max value; no data removed.")
         return time_series_df
 
-    # Remove the windows
+    # remove the windows
     time_series_df = time_series_df[
         (minute_of_day < windows[0][day_of_year - 1]) |
         (minute_of_day > windows[1][day_of_year - 1])
@@ -136,6 +186,64 @@ def remove_clipping_with_flexible_window(
               "% of the data has been removed!")
 
     return time_series_df
+
+
+def return_flexible_clipping_window(
+    time_series_df, verbose=False, max_power=None
+):
+    """return universal clipping window of power signal.
+
+    The function determines a time window for each day <day> of the year, so
+    that for every minute <min> in the time window there is year in the time
+    series such that <min>-th minute of the <day>-th day of that year hits the
+    maximum power.
+
+    Args:
+        time_series_df (Pandas DataFrame): with a time stamp (datetime) as an
+            index, a "Power" column (float), and a "minute_of_day" (int) column
+        verbose (bool): Optional; print additional info if true
+        max_power (float): Optional; specify maximal power
+
+    Returns:
+        windows (list): a list containing two lists: one list with the lower
+            bounds and one list with the upper bounds of the windows of each
+            day in a year
+    """
+    if not max_power:
+        max_power = time_series_df.Power.max()
+        if verbose:
+            print("Max power set as "+str(max_power))
+
+    day_of_year = np.array(time_series_df.index.dayofyear)
+    minute_of_day = np.array(time_series_df.minute_of_day)
+    power = np.array(time_series_df.Power)
+    windows = np.zeros((2, 366))
+    found_non_empty_window = False
+
+    # To measure at the end how much data is removed
+    if verbose:
+        initial_size = len(time_series_df)
+
+    for day in range(0, 366):
+        clipping_df = time_series_df[
+                (day_of_year == day + 1) &
+                (power == max_power)
+            ]
+        if clipping_df.empty:
+            windows[0][day] = -2
+            windows[1][day] = -1
+        else:
+            windows[0][day] = clipping_df.minute_of_day.min()
+            windows[1][day] = clipping_df.minute_of_day.max()
+            found_non_empty_window = True
+
+    if not found_non_empty_window:
+        # no non empty window was found
+        if verbose:
+            print("power never exceeds max value; no data removed.")
+        return time_series_df
+
+    return windows
 
 def downsample_dataframe(df,
                          offset='D',
