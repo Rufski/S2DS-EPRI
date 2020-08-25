7136d76f90f14d28072bb185cc285c5e82777b72
Ubuntu <maria@S2DSVM13.oo0px1rf2byuzixl1gjp2lf2nf.ax.internal.cloudapp.net>
2020-08-25 14:57:27 +0000

    Updated python scripts for automatizing CODS analysis

--- src/models/perform_cods_analysis.py
+++ src/models/perform_cods_analysis.py
@@ -1,4 +1,9 @@
 from  cods_analysis import CODS_with_bootstrap
+import sys
 
+if __name__ == "__main__":
 
-if __main__: 
+    synth_type = sys.argv[1]
+
+    for i in range(0, 50):
+        CODS_with_bootstrap(synth_type, index=i, realizations=512, verbose=True)
