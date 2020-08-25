eec20b1c20cf31f55d47953dd83fd87b7b5a7471
Ubuntu <maria@S2DSVM13.oo0px1rf2byuzixl1gjp2lf2nf.ax.internal.cloudapp.net>
2020-08-25 18:30:45 +0000

    included updated PI dfs and included type of clipping removal option in cods_with_bootstrap function

--- src/models/perform_cods_analysis.py
+++ src/models/perform_cods_analysis.py
@@ -1,9 +1,10 @@
-from  cods_analysis import CODS_with_bootstrap
+from cods_analysis import cods_with_bootstrap
 import sys
 
 if __name__ == "__main__":
 
     synth_type = sys.argv[1]
 
-    for i in range(0, 50):
-        CODS_with_bootstrap(synth_type, index=i, realizations=512, verbose=True)
+    for i in range(0, 25):
+        print ("running CODS algorithm for %i time series" %i)
+        cods_with_bootstrap(synth_type, index=i, realizations=16, clipping="basic", verbose=True)
