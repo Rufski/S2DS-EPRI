988943a88a960d66fa45d8dd10165c00bde3644b
cjochum <clemens.jochum@tuwien.ac.at>
2020-08-26 19:40:04 +0200

    cods analysis scripts

--- src/models/cods_analysis.py
+++ src/models/cods_analysis.py
@@ -2,14 +2,17 @@ from src.data.import_data import import_df_from_zip_pkl
 import rdtools
 import pickle
 import time
+import numpy as np
 
 
-def cods_with_bootstrap(synth_type, index=0, realizations=512, clipping="basic", verbose=False):
+def cods_with_bootstrap(synthetic_type, index=0, realizations=512, clipping="basic", extra=False, outlier_threshold=0., verbose=False):
+    
     """
     
-    """  
+    """
+
     # Load datasets
-    path_to_zip_pkl_pi = "../../data/raw/synthetic_" + synth_type + "_pi_daily.zip"
+    path_to_zip_pkl_pi = f'../../data/raw/synthetic_{synthetic_type:s}_pi_daily.zip'
     try:
         df = import_df_from_zip_pkl(path_to_zip_pkl_pi, index=index, verbose=True, 
                                     minofday=False)
@@ -21,25 +24,35 @@ def cods_with_bootstrap(synth_type, index=0, realizations=512, clipping="basic",
     
     # Initialize instance
     if clipping=="basic":
+        cods_n = 1
+        print(df.PI_clipping_basic.isna().sum())
+        df[df.PI_clipping_basic<outlier_threshold] = np.nan
+        print(df.PI_clipping_basic.isna().sum())
         cods_instance = rdtools.soiling.cods_analysis(df.PI_clipping_basic)
     elif clipping=="flexible":
+        cods_n = 2
+        df[df.PI_clipping_flexible<outlier_threshold] = np.nan
         cods_instance = rdtools.soiling.cods_analysis(df.PI_clipping_flexible)
-    elif clippling=="universal":
+    elif clipping=="universal":
+        cods_n = 3
+        df[df.PI_clipping_universal<outlier_threshold] = np.nan
         cods_instance = rdtools.soiling.cods_analysis(df.PI_clipping_universal)
     else:
         if verbose==True:
             print("Function for removing clipping not implemented!")
-    # run algoritm
+
+    # run algorithm
     cods_instance.run_bootstrap(realizations, verbose=verbose)
     
     end_time = time.time() # remove?
     print("--- %s min ---" %((end_time - start_time)/60.)) # remove?
     
     # save results
-    _file = open("../../data/processed/cods_results_" + synth_type + "_" + str(index) + "_" + str(realizations) + ".pkl", "wb")
+    type_name = (synthetic_type, synthetic_type + '_extra')[extra == True] 
+    _file = open(f'../../data/processed/cods_results_{synthetic_type:s}_i{str(index).zfill(3):s}_r{str(realizations).zfill(3):s}_c{cods_n:d}.pkl', "wb")
     pickle.dump(cods_instance , _file)
     
-    return
+    return None
 
 def load_cods_results(synth_type, index=0, realizations=512, verbose=False):
     """
