9dfcfad6f9553b5b918dcdc938875ababebc4a15
clemens <c.jochum@gmail.com>
2020-08-26 21:33:19 +0200

    fixed bug in performance index

--- src/features/performance_index.py
+++ src/features/performance_index.py
@@ -15,7 +15,6 @@ def normalize_power_signal(
         data_frame,
         poa_reference=None,
         clearsky=True,
-        nighttime=True,
         clipping='basic',
         outlier_threshold=0.0,
         verbose=False):
@@ -42,52 +41,37 @@ def normalize_power_signal(
         data_frame.POA.to_numpy() / 1000.
     t_factor = 1 + gamma * (t_cell - t_cell_shift)
 
-    # calculate clipping mask for throwing away clipped data afterward
-    if clipping == 'universal':
-        clipping_window_limits = return_universal_clipping_window(data_frame)
-    elif clipping == 'flexible':
-        clipping_window_limits = return_flexible_clipping_window(data_frame)
+    # calculate expected power and normalize
+    p_expected = poa_reference * p_ref / 1000. * t_factor
+    p_norm = data_frame.Power / p_expected
 
     # throw away cloudy periods
     if clearsky is True:
         clearsky_mask = detect_clearsky(data_frame.POA, poa_reference,
                                         data_frame.index, clearsky_window_size)
-        data_frame = data_frame[clearsky_mask]
-        if verbose is True:
-            print('{:.2f} % of data remaining after clearsky '
-                  'detection.'.format(data_frame.Power.size / data_size_init))
-
-    # throw away clipping data
-    if clipping == 'basic':
-        data_frame = data_frame[data_frame.Power < 1827.0]
-    elif clipping == 'flexible':
-        day_of_year = data_frame.index.dayofyear.to_numpy()
-        data_frame = data_frame[
-            (data_frame.minute_of_day < clipping_window_limits[0][day_of_year - 1]) |
-            (data_frame.minute_of_day > clipping_window_limits[1][day_of_year - 1])
-            ]
-    elif clipping == 'universal':
-        data_frame = data_frame[
-            (data_frame.minute_of_day < clipping_window_limits[0]) |
-            (data_frame.minute_of_day > clipping_window_limits[1])
-            ]
-    if (clipping is not None) and (verbose is True):
-        print('{:.2f} % of data remaining after clipping '
-              'removal.'.format(data_frame.Power.size / data_size_init))
+    else:
+        clearsky_mask = np.full(data_frame.POA.size, True)
 
     # throw away nighttime data
-    if nighttime is True:
-        data_frame = remove_night_time_data(data_frame)
-        if verbose is True:
-            print('{:.2f} % of data remaining after night-time '
-                  'removal.'.format(data_frame.Power.size / data_size_init))
+    nighttime_mask = data_frame[clearsky_mask].Power != -1.
 
-    # calculate expected power and normalize
-    if clearsky is True:
-        poa_reference = poa_reference[clearsky_mask]
-        t_factor = t_factor[clearsky_mask]
-    p_expected = poa_reference * p_ref / 1000. * t_factor
-    p_norm = data_frame.Power / p_expected
+    # calculate clipping mask for throwing away clipped data afterward
+    if clipping == 'basic':
+        data_frame_temp = data_frame[clearsky_mask]
+        data_frame_temp = data_frame_temp[nighttime_mask]
+        clipping_mask = data_frame_temp.Power < 1800.0
+
+    # throw away cloudy, nighttime, and clipped periods
+    p_norm = p_norm[clearsky_mask]
+    poa_reference = poa_reference[clearsky_mask]
+    p_norm = p_norm[nighttime_mask]
+    poa_reference = poa_reference[nighttime_mask]
+    p_norm = p_norm[clipping_mask]
+    poa_reference = poa_reference[clipping_mask]
+
+    if verbose is True:
+        print(f'{p_norm.size/data_size_init:.2f} % of data remaining after '
+              'clearsky-detection, nighttime-removal, and clipping-removal')
 
     # calculate daily aggregate with POA as weight function
     p_norm_daily = p_norm * poa_reference
